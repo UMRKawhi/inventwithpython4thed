c775a75e4a2830d30eee3292bc6e3620aefc25b9
UMRKawhi <48422665+UMRKawhi@users.noreply.github.com>
2020-11-04 21:20:34 +1100

    finished the dragon games

--- src/dragon.py
+++ src/dragon.py
@@ -10,4 +10,33 @@ def displayIntro():
 
 def chooseCave():
     cave = ''
-    while cave != '1' and
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+        return cave
+
+def checkCave(chooseCave):
+    print('You approach the cave...')
+    time.sleep(2)
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! He opens his jaws \
+        and...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+    
+    if chosenCave == str(friendlyCave):
+        print('Gives you his treasure!')
+    else:
+        print('Gobbles you down in one bite!')
+
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+    displayIntro()
+    caveNumber = chooseCave()
+    checkCave(caveNumber)
+
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
