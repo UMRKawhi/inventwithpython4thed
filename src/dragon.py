d897521e260b8272840bfbf79a962e0fb415a765
UMRKawhi <48422665+UMRKawhi@users.noreply.github.com>
2020-11-05 12:26:16 +1100

    add comment

--- src/dragon.py
+++ src/dragon.py
@@ -9,6 +9,7 @@ def displayIntro():
     print()
 
 def chooseCave():
+    # input validation
     cave = ''
     while cave != '1' and cave != '2':
         print('Which cave will you go into? (1 or 2)')
